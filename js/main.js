75a35e66a5a8903aeebceb648548fa0eb091219a
mhamdy88 <m.hamdy88@gmail.com>
2018-02-20 22:11:31 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+    AOS.init();
+    setTimeout(function () {
+        $(".topBar").slideDown("slow");
+    }, 500);
+
+    $('.menuButton').click(function () {
+        $('.menuOverlay').slideToggle(500);
+    });
+
+    $(document).keyup(function (e) {
+        if (e.keyCode == 27) { // escape key maps to keycode `27`            
+            $(".menuOverlay").slideUp();
+        }
+    });
+
+    $('#mnuService').mouseout(function () {
+        //$('#pnlService').hide();
+    });
+    $('#mnuService').mouseenter(function () {
+        $('#pnlService').show();
+    });
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 40) {
+            $('.topBar').addClass('topBar_scroll');
+        } else { $('.topBar').removeClass('topBar_scroll', 300); }
+    });
+
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        autoplay: {
+            delay: 3000,
+        },
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+
+
+    var flipper = $("#card").flip({
+        trigger: 'manual',
+        axis: 'x'
+    });
+    $('#card .content').hide();
+    $('.serviceBlock.mobileApp').click(function () {
+        var content = $('.mobileAppContent').html();
+        $('.back').html(content);
+        $("#card").flip('toggle');
+    })
+    $('.serviceBlock.webApp').click(function () {
+        var content = $('.webAppContent').html();
+        $('.back').html(content);        
+        flipper.flip(true);
+    })
+    $('.serviceBlock.startup').click(function () {
+        var content = $('.startUpContent').html();
+        $('.back').html(content);        
+        flipper.flip(true);
+    })
+    $('.serviceBlock.consult').click(function () {
+        var content = $('.consultContent').html();
+        $('.back').html(content);        
+        flipper.flip(true);
+    })
+    $('#card .back').click(function () {
+        $("#card").flip('toggle');
+    })
+});
